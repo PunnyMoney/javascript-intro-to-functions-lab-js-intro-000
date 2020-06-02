0a53209a94cf24a8091482592c79fbab0bab550f
Penelope Jungreis <penelope.jungreis@gmail.com>
2020-06-02 14:06:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ function whisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
-    return "I can't hear you!";
+    return 'I can\'t hear you!';
   } else if (string === string.toUpperCase) {
-    return "YES INDEED!";
+    return 'YES INDEED!';
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
+console.log(shout('hello'));
+console.log(whisper('Hello'));
+console.log(sayHiToGrandma('hi grandma'));
